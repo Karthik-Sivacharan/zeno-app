96ffb2eceb6ec464285353a8d3868ba2f6ad6b7e
Karthik-Sivacharan <karthiksivacharan@gmail.com>
2025-11-21 18:31:07 -0800

    Initial Commit

---
+++ Zeno/ZenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenoApp.swift
+//  Zeno
+//
+//  Created by Karthik Sivacharan on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ZenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZenoTests/ZenoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZenoTests.swift
+//  ZenoTests
+//
+//  Created by Karthik Sivacharan on 11/21/25.
+//
+
+import Testing
+@testable import Zeno
+
+struct ZenoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
